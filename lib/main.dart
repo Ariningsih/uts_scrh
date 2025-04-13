d7a05b73c0dbf0b2521240a227d939f26d1c5637
Ariningsih <162403769+Ariningsih@users.noreply.github.com>
2025-04-13 09:10:20 +0800

    Search Page

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'search_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Movie Search',
+      theme: ThemeData.dark(),
+      home: SearchPage(),
+    );
+  }
+}
